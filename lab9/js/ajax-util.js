c9a9a379f657312f57d8b0c64fd073a887f098ff
tinaklsnk <tinaklsnk@github.com>
2022-06-01 03:42:36 +0200

    first commit lab9

---
+++ lab9/js/ajax-util.js
@@ -0,0 +1,33 @@
+(function (global) {
+    let ajaxUtils = {}
+
+    function getRequestObject() {
+        if (global.XMLHttpRequest) {
+            return new XMLHttpRequest()
+        } else {
+            console.error('Ajax is not supported')
+            return null
+        }
+    }
+
+    ajaxUtils.sendGetRequest =
+        function (requestUrl, responseHandler) {
+            let request = getRequestObject();
+            request.onreadystatechange =
+                function() {
+                handleResponse(request, responseHandler);
+            };
+            request.open('GET', requestUrl, true);
+            request.send(null);
+        }
+
+    function handleResponse(request,
+                            responseHandler) {
+        if ((request.readyState === 4) &&
+            (request.status === 200)) {
+            responseHandler(request);
+        }
+    }
+
+    global.ajaxUtils = ajaxUtils;
+})(window)
